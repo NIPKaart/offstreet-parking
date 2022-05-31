2bcfedbc116b54dfc48615242c1c46e5a113f5ad
Klaas Schoute <hello@student-techlife.com>
2022-05-31 02:27:59 +0200

    Add the country and province ID to commit

--- cities/amsterdam.py
+++ cities/amsterdam.py
@@ -17,27 +17,27 @@ def check_value(value):
     else:
         return value
 
-def purge_database(city, time):
+def purge_database(municipality, time):
     """Purge the database tabel."""
     print(f'{time} - START met leeggooien van de database')
     try:
-        sql = "DELETE FROM `parking_garages` WHERE `city`=%s"
-        cursor.execute(sql, city)
+        sql = "DELETE FROM `parking_garages` WHERE `municipality`=%s"
+        cursor.execute(sql, municipality)
         connection.commit()
     except Exception as e:
         print(f'MySQL error: {e}')
     finally:
         print(f'{time} - Klaar met leegmaken van de database')
 
-def update_database(data_set, city, time):
+def update_database(data_set, municipality, time):
     """Update the database with new data."""
-    # purge_database(city)
+    # purge_database(municipality, time)
     print(f'{time} - START bijwerken van database met nieuwe data')
     count=1
     try:
         for item in data_set:
-            sql = """INSERT INTO `parking_garages` (id, name, city, state, free_space_short, free_space_long, short_capacity, long_capacity, longitude, latitude, visibility, created_at, updated_at) 
-                     VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) ON DUPLICATE KEY 
+            sql = """INSERT INTO `parking_garages` (id, name, country_id, province_id, municipality, state, free_space_short, free_space_long, short_capacity, long_capacity, longitude, latitude, visibility, created_at, updated_at) 
+                     VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) ON DUPLICATE KEY 
                      UPDATE id=values(id),
                             name=values(name),
                             state=values(state),
@@ -48,7 +48,7 @@ def update_database(data_set, city, time):
                             longitude=values(longitude),
                             latitude=values(latitude),
                             updated_at=values(updated_at)"""
-            val = (count, str(item.garage_name), str(city) ,str(item.state), check_value(item.free_space_short), check_value(item.free_space_long), check_value(item.short_capacity), check_value(item.long_capacity), float(item.longitude), float(item.latitude), bool(True), (datetime.datetime.now()), (datetime.datetime.now()))
+            val = (count, str(item.garage_name), int(157), int(8), str(municipality) ,str(item.state), check_value(item.free_space_short), check_value(item.free_space_long), check_value(item.short_capacity), check_value(item.long_capacity), float(item.longitude), float(item.latitude), bool(True), (datetime.datetime.now()), (datetime.datetime.now()))
             cursor.execute(sql, val)
             count+=1
         connection.commit()
