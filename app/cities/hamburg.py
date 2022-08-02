4948b86b21ace0f7a7ec109f6e0b5af8c6b272f3
Klaas Schoute <hello@student-techlife.com>
2022-08-02 02:19:17 +0200

    Change location_id for Hamburg

---
+++ app/cities/hamburg.py
@@ -0,0 +1,62 @@
+"""Python script for Park and Ride Hamburg data."""
+import datetime
+import json
+
+from hamburg import ParkAndRide, UDPHamburg
+
+from app.database import connection, cursor
+
+
+async def async_get_parking(bulk="false"):
+    """Get parking data from API."""
+    async with UDPHamburg() as client:
+        parking: ParkAndRide = await client.park_and_ride(bulk=bulk)
+        return parking
+
+
+def update_database(data_set, municipality, time):
+    """Update the database with new data."""
+    # purge_database(municipality, time)
+    print(f"{time} - START bijwerken van database met nieuwe data")
+    try:
+        for item in data_set:
+            location_id = (
+                f"{item.spot_id[0:5]}_{item.name.replace(' ', '_')}_{item.capacity}"
+            )
+            location_id = location_id.replace(".", "-")
+            sql = """INSERT INTO `parking_garages` (id, name, country_id, province_id, municipality, free_space_short, short_capacity, availability_pct, prices, url, longitude, latitude, visibility, created_at, updated_at)
+                     VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) ON DUPLICATE KEY
+                     UPDATE id=values(id),
+                            name=values(name),
+                            state=values(state),
+                            free_space_short=values(free_space_short),
+                            short_capacity=values(short_capacity),
+                            availability_pct=values(availability_pct),
+                            prices=values(prices),
+                            longitude=values(longitude),
+                            latitude=values(latitude),
+                            updated_at=values(updated_at)"""
+            val = (
+                location_id.replace("__", "_"),
+                str(item.name),
+                int(83),
+                int(14),
+                str(municipality),
+                item.free_space,
+                item.capacity,
+                item.availability_pct,
+                json.dumps(item.tickets),
+                item.url,
+                float(item.longitude),
+                float(item.latitude),
+                bool(True),
+                (datetime.datetime.now()),
+                item.updated_at,
+            )
+            # print(val)
+            cursor.execute(sql, val)
+        connection.commit()
+    except Exception as error:
+        print(f"MySQL error: {error}")
+    finally:
+        print(f"{time} - KLAAR met updaten van database")
