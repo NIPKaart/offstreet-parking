aa3804af9ded062c584793e0305e281e36216692
Klaas Schoute <hello@student-techlife.com>
2023-07-02 00:52:42 +0200

    Refactor to general city class

--- app/cities/__init__.py
+++ app/cities/__init__.py
@@ -1,1 +1,22 @@
 """General class for cities."""
+
+
+class City:
+    """General class for cities."""
+
+    def __init__(  # noqa: PLR0913
+        self,
+        name: str,
+        country: str,
+        country_id: int,
+        province_id: int,
+        geo_code: str,
+        phone_code: str,
+    ) -> None:
+        """Initialize the class."""
+        self.name = name
+        self.country = country
+        self.country_id = country_id
+        self.province_id = province_id
+        self.geo_code = geo_code
+        self.phone_code = phone_code
--- main.py
+++ main.py
@@ -8,49 +8,78 @@ from pathlib import Path
 import pytz
 from dotenv import load_dotenv
 
-from app.cities import amsterdam, hamburg
+from app.cities.germany import hamburg
+from app.cities.netherlands import amsterdam
 from app.database import test_connection
 from app.helpers import test_data
 
-load_dotenv()
-env_path = Path(".") / ".env"
-load_dotenv(dotenv_path=env_path)
-
 CITY = os.getenv("CITY")
 WAIT_TIME = int(os.getenv("WAIT_TIME"))
-
 TESTING = False
 
 
+class CityProvider:
+    """Class to provide the correct city."""
+
+    def provide_city(self, city_name: str) -> object:
+        """Provide the correct city.
+
+        Args:
+        ----
+            city_name (str): The city to provide.
+
+        Returns:
+        -------
+            city_class (class): The class of the city.
+        """
+        match city_name:
+            case "amsterdam":
+                city_class = amsterdam.Municipality()
+            case "hamburg":
+                city_class = hamburg.Municipality()
+            case _:
+                msg = f"{city_name} is not a valid city."
+                raise ValueError(msg)
+        return city_class
+
+
 if __name__ == "__main__":
-    print("--- Start upload program ---")
+    # Load environment variables
+    load_dotenv()
+    env_path = Path(".") / ".env"
+    load_dotenv(dotenv_path=env_path)
+
+    cp = CityProvider()
+    print("--- Start program ---")
+    print()
+
+    # Get the city from the environment variables
+    selected_city: str = os.getenv("CITY").lower()
+    provided_city = cp.provide_city(selected_city)
+
     if TESTING:
-        if CITY == "Amsterdam":
-            data = asyncio.run(amsterdam.async_get_garages())
-            test_data(data)
-        elif CITY == "Hamburg":
-            data = asyncio.run(hamburg.async_get_parking())
-            test_data(data)
+        data_set = asyncio.run(provided_city.async_get_locations())
+        test_data(data_set)
         test_connection()
     else:
         while True:
-            CURRENT_TIME = datetime.now(tz=pytz.timezone("Europe/Amsterdam")).strftime(
-                "%H:%M:%S",
-            )
-            print(f"-------- START {CITY} ---------")
-            if CITY == "Amsterdam":
-                data = asyncio.run(amsterdam.async_get_garages())
-                amsterdam.update_database(data, CITY, CURRENT_TIME)
-            elif CITY == "Hamburg":
-                local_zone = pytz.timezone("Europe/Amsterdam")
-                local_time: datetime = datetime.now(tz=local_zone)
+            LOCAL_ZONE = pytz.timezone("Europe/Amsterdam")
+            CURRENT_TIME = datetime.now(tz=LOCAL_ZONE).strftime("%H:%M:%S")
+            print(f"-------- START {selected_city} ---------")
 
+            # Get the data from the selected city
+            data_set = asyncio.run(provided_city.async_get_locations())
+
+            # Upload the data to the database
+            if selected_city in ["hamburg"]:
+                local_time: datetime = datetime.now(tz=LOCAL_ZONE)
                 if local_time.hour >= 1:
-                    data = asyncio.run(hamburg.async_get_parking(bulk="true"))
-                    hamburg.update_database(data, CITY, CURRENT_TIME)
+                    provided_city.upload_data(data_set, CURRENT_TIME)
                 else:
                     print(
                         "Hamburg: Not updating database, time between 00:00 and 01:00.",
                     )
-            print(f"--------- DONE {CITY} ---------")
+            else:
+                provided_city.upload_data(data_set, CURRENT_TIME)
+            print(f"--------- DONE {selected_city} ---------")
             time.sleep(60 * WAIT_TIME)
